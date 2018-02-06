99068b913f7764dc0bd73d2d47387c58cfcedc57
wlafrance <warren.lafrance@gmail.com>
2018-02-06 14:13:06 -0600

    Added code while at work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  pageTitle: string = 'Product Listing';
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
